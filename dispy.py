94477653f94d2d76f5c7a4f290e1579c20638864
saadati944 <saadati944@gmail.com>
2020-09-07 09:40:44 +0430

    send welcome message to newly joined members

--- dispy.py
+++ dispy.py
@@ -25,6 +25,7 @@ async def on_ready():
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(f'Hi {member.name}, welcome to my Discord server!')
+    await member.dm_channel.send(data.get_static_data('welcome'))
 
 
 @client.event
