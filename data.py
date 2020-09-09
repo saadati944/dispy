5a3b14eff2ba56503f44496bc26a8e7bc4234d9f
saadati944 <saadati944@gmail.com>
2020-09-09 10:16:35 +0430

    add user parameter to check if the request is from the user or not. if True then check the file and if that static file starts with "hidden", don't read it for user. change return type to a list of strings (return lines).

--- data.py
+++ data.py
@@ -8,9 +8,16 @@ if os.name=='nt':
     slash='\\'
 
 
-def get_static_data(name):
+
+#this function return an array of lines !!!
+def get_static_data(name, user=False):
     try:
         assert os.path.exists(config.static_data+slash+name)
-        return open(config.static_data+slash+name,'r',encoding='utf-8').read()
+        if not user:
+            return open(config.static_data+slash+name,'r',encoding='utf-8').readlines()
+        else :
+            res=open(config.static_data+slash+name,'r',encoding='utf-8').readlines()
+            assert len(res)>0 and not res[0].startswith('hidden')
+            return res
     except:
-        return f"can't find static data '{name}'"
+        return [f"can't find static data '{name}'"]
